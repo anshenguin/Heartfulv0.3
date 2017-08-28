5c17d6e974ad4025a3d081d375cd07e73538547f
Gulshan Kumar <k2caher@gmail.com>
2017-08-28 14:45:17 +0530

    Fragment One finished
    :100:

--- app/src/main/java/com/example/hp/heartful/OrgInfo.java
+++ app/src/main/java/com/example/hp/heartful/OrgInfo.java
@@ -4,71 +4,113 @@ package com.example.hp.heartful;
  * Created by HP INDIA on 10-Apr-17.
  */
 public class OrgInfo {
-    /** Default translation for the word */
+
+    public OrgInfo() {}
+
+
+    private String mImage;
+    //    /** Default translation for the word */
     private String mOrgname;
 
+    public String getmCategory() {
+        return mCategory;
+    }
+
+    public void setmCategory(String mCategory) {
+        this.mCategory = mCategory;
+    }
+
+    private String mCategory;
     /** Miwok translation for the word */
     private String mOrginfo;
 
-    /** Image resource ID for the word */
-    private int mImageResourceId = NO_IMAGE_PROVIDED;
-
-    /** Constant value that represents no image was provided for this word */
-    private static final int NO_IMAGE_PROVIDED = -1;
-    public OrgInfo(){}
-
-    /**
-     * Create a new Word object.
-     *
-     * @param defaultTranslation is the word in a language that the user is already familiar with
-     *                           (such as English)
-     * @param miwokTranslation is the word in the Miwok language
-     */
-//    public OrgInfo(String defaultTranslation, String miwokTranslation) {
-//        mOrgname = defaultTranslation;
-//        mOrginfo = miwokTranslation;
-//    }
-
-    /**
-     * Create a new Word object.
-     *
-     * @param defaultTranslation is the word in a language that the user is already familiar with
-     *                           (such as English)
-     * @param miwokTranslation is the word in the Miwok language
-     * @param imageResourceId is the drawable resource ID for the image associated with the word
-     *
-     */
-    public OrgInfo(String defaultTranslation, String miwokTranslation, int imageResourceId) {
-        mOrgname = defaultTranslation;
-        mOrginfo = miwokTranslation;
-        mImageResourceId = imageResourceId;
-    }
-
-    /**
-     * Get the default translation of the word.
-     */
-    public String getOrgname() {
+    public String getmOrgname() {
         return mOrgname;
     }
 
-    /**
-     * Get the Miwok translation of the word.
-     */
-    public String getOrginfo() {
+    public void setmOrgname(String mOrgname) {
+        this.mOrgname = mOrgname;
+    }
+
+    public String getmOrginfo() {
         return mOrginfo;
     }
 
-    /**
-     * Return the image resource ID of the word.
-     */
-    public int getImageResourceId() {
-        return mImageResourceId;
+    public void setmOrginfo(String mOrginfo) {
+        this.mOrginfo = mOrginfo;
     }
 
-    /**
-     * Returns whether or not there is an image for this word.
-     */
-    public boolean hasImage() {
-        return mImageResourceId != NO_IMAGE_PROVIDED;
+    public String getmImage() {
+        return mImage;
     }
+
+    public void setmImage(String mImage) {
+        this.mImage = mImage;
+    }
+
+
+
+
+//    /** Image resource ID for the word */
+//    private int mImageResourceId = NO_IMAGE_PROVIDED;
+//
+//    /** Constant value that represents no image was provided for this word */
+//    private static final int NO_IMAGE_PROVIDED = -1;
+//    public OrgInfo(){}
+//
+//    /**
+//     * Create a new Word object.
+//     *
+//     * @param defaultTranslation is the word in a language that the user is already familiar with
+//     *                           (such as English)
+//     * @param miwokTranslation is the word in the Miwok language
+//     */
+////    public OrgInfo(String defaultTranslation, String miwokTranslation) {
+////        mOrgname = defaultTranslation;
+////        mOrginfo = miwokTranslation;
+////    }
+//
+//    /**
+//     * Create a new Word object.
+//     *
+//     * @param defaultTranslation is the word in a language that the user is already familiar with
+//     *                           (such as English)
+//     * @param miwokTranslation is the word in the Miwok language
+//     * @param imageResourceId is the drawable resource ID for the image associated with the word
+//     *
+//     */
+//    public OrgInfo(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+//        mOrgname = defaultTranslation;
+//        mOrginfo = miwokTranslation;
+//        mImageResourceId = imageResourceId;
+//    }
+//
+//    /**
+//     * Get the default translation of the word.
+//     */
+//    public String getOrgname() {
+//        return mOrgname;
+//    }
+//
+//    /**
+//     * Get the Miwok translation of the word.
+//     */
+//    public String getOrginfo() {
+//        return mOrginfo;
+//    }
+//
+//    /**
+//     * Return the image resource ID of the word.
+//     */
+//    public int getImageResourceId() {
+//        return mImageResourceId;
+//    }
+//
+//    /**
+//     * Returns whether or not there is an image for this word.
+//     */
+//    public boolean hasImage() {
+//        return mImageResourceId != NO_IMAGE_PROVIDED;
+//    }
+//}
 }
