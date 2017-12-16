8eb25a485483bcd9d581b8b26703cac5e77f5f80
Anshul <anshul.anshul.rawat@gmail.com>
2017-12-16 12:15:52 +0530

    Pre Alpha push #5 dialog box fixed

---
+++ app/src/main/java/com/kinitoapps/ngolink/FireMissilesDialogFragment.java
@@ -0,0 +1,16 @@
+//package com.kinitoapps.ngolink;
+//
+//import android.app.AlertDialog;
+//import android.app.Dialog;
+//import android.app.DialogFragment;
+//import android.content.DialogInterface;
+//import android.os.Bundle;
+//
+///**
+// * Created by HP INDIA on 16-Dec-17.
+// */
+//
+//public class FireMissilesDialogFragment extends DialogFragment {
+//    @Override
+//
+//}
